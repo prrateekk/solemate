493488b432343a2c584837291eac338f9c688f85
Prateek <pagarwal@rippling.com>
2022-10-30 01:07:51 +0530

    Adding queryset on params

--- solemate/solemate/urls.py
+++ solemate/solemate/urls.py
@@ -13,12 +13,16 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.urls import path, include
 from django.contrib import admin
-from django.urls import path
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'user', views.UserViewSet, basename='user')
+
 urlpatterns = [
+    path('', include(router.urls)),
     path("admin/", admin.site.urls),
-    path('', views.index, name='index'),
     path('profile', views.profile, name='profile')
 ]
--- solemate/solemate/views.py
+++ solemate/solemate/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse
 from solemate.models import User
-
+from rest_framework import viewsets
+from rest_framework_mongoengine.serializers import DocumentSerializer
 
 def index(request):
     return HttpResponse("Hello, world. You're at the app index. What's going on?")
@@ -9,3 +10,34 @@ def index(request):
 def profile(request):
     user = User.objects.all().first()
     return HttpResponse("Hello, {}. How's going on?".format(user.name))
+
+class UserSerializer(DocumentSerializer):
+    class Meta:
+        model = User
+        fields = "__all__"
+
+class SMModelViewSet(viewsets.ModelViewSet):
+    def getValInFieldType(self, val, key, field):
+        python_value = field.to_python(val)
+        return python_value
+
+    def updateQuerySet(self, qs, key, val):
+        field = self.model._fields.get(key)
+        if not field:
+            return qs
+        python_value = self.getValInFieldType(val, key, field)
+        qs = qs.filter(**{key: python_value})
+        return qs
+
+    def get_queryset(self):
+        assert self.queryset is None
+        qs = self.model.objects
+        print(self.request.query_params)
+        for key, val in self.request.query_params.items():
+            qs = self.updateQuerySet(qs, key, val)
+        return qs
+
+class UserViewSet(SMModelViewSet):
+    model = User
+    serializer_class = UserSerializer
+
